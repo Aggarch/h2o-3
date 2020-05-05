e9ebe95b005b1cd818fba4046de70f3af4750a59
wendycwong <wendycwong@yahoo.com>
2020-05-05 14:44:18 -0700

    Added pyunit test to verify correct alpha/lambda array value search.

---
+++ h2o-py/tests/testdir_algos/glm/pyunit_PUBDEV_7481_lambda_alpha_array.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.insert(1,"../../../")
+import h2o
+from builtins import range
+from tests import pyunit_utils
+from h2o.estimators.glm import H2OGeneralizedLinearEstimator as glm
+
+# Given arrays of lambda and alpha, can we get access to the best model found?
+def glm_alpha_lambda_arrays():
+    # read in the dataset and construct training set (and validation set)
+    d = h2o.import_file(path=pyunit_utils.locate("smalldata/logreg/prostate.csv"))
+    m = glm(family='binomial',Lambda=[0.1,0.5,0.9], alpha=[0.1,0.5,0.9])
+    m.train(training_frame=d,x=[2,3,4,5,6,7,8],y=1)
+    r = glm.getGLMRegularizationPath(m)
+    print(m)
+
+if __name__ == "__main__":
+    pyunit_utils.standalone_test(glm_alpha_lambda_arrays)
+else:
+    glm_alpha_lambda_arrays()
